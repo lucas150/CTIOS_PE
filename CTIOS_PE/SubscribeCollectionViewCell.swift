16671ce74217d13b271efe605ae69e0419bb5a39
Henil Gandhi <henil.gandhi2003@gmail.com>
2025-08-07 16:50:38 +0530

    True Money DEMO

---
+++ CTIOS_PE/SubscribeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SubscribeCollectionViewCell.swift
+//  CTIOS_PE
+//
+//  Created by Henil Gandhi on 07/08/25.
+//
+
+import UIKit
+
+class SubscribeCollectionViewCell: UICollectionViewCell {
+    
+}
