1bbd7dc6f86ccdbd8ba633100f5a43d81a381407
Pranjal <pranjal@clevertap.com>
2025-08-14 11:38:29 +0530

    TrueMoney changes

---
+++ CTIOS_PE/TrueMoneyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TrueMoneyCollectionViewCell.swift
+//  CTIOS_PE
+//
+//  Created by Pranjal Choudhary on 14/08/25.
+//
+
+import UIKit
+
+class TrueMoneyCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CTIOS_PE/TrueMoneyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrueMoneyViewController.swift
+//  CTIOS_PE
+//
+//  Created by Pranjal Choudhary on 14/08/25.
+//
+
+import UIKit
+
+class TrueMoneyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
